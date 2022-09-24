a0ffb52f99733a1063fa7e363a3e461b4344dbf3
Matthew Cook <cookm353@gmail.com>
2022-09-24 16:30:48 -0400

    updating repo

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 """Blogly application."""
 
 from flask import Flask, render_template, redirect, flash, session, request
-from models import db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_sqlalchemy import SQLAlchemy
+from models import db, connect_db, User
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = 'bloggo'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
-db.create_all()
-
 
 @app.route('/')
 def index():
@@ -37,3 +40,12 @@ def add_user():
 @app.route('/users/new', methods=['POST'])
 def create_user():
     ...
+    
+    
+def main():
+    # db.create_all()
+    print(db)
+    print(db.app)
+    
+if __name__ == "__main__":
+    main()
