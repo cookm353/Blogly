030929e21067fd3c3a992a47bbf90d94cbc5b21a
Matthew Cook <cookm353@gmail.com>
2022-10-02 11:18:35 -0400

    fixed seed script

--- seed.py
+++ seed.py
@@ -1,4 +1,4 @@
-from models import User, db
+from models import User, Post, db
 from app import app
 
 
@@ -16,11 +16,34 @@ dave_grohl = User(first_name='Dave', last_name='Grohl',
             img_url='static/dave_grohl.png')
 warren = User(first_name='Elizabeth', last_name='Warren',
             img_url='static/elizabeth_warren.jpg')
+doe = User(first_name='Jane', last_name='Doe')
 
 # Add new users to session
 db.session.add(jackie_chan)
 db.session.add(dave_grohl)
 db.session.add(warren)
+db.session.add(doe)
+
+# Commit to DB
+db.session.commit()
+
+# Ensure Posts table is empty
+Post.query.delete()
+
+# Add starting users
+doe_post = Post(title='First Post!', 
+                 content='Hello world!', user_id=4)
+grohl_post = Post(title='Newest Album',
+                  content='Check out our latest album, Medicine at Midnight!',
+                  user_id=2)
+warren_post = Post(title='Have a Problem?',
+                   content='I have a plant for that!',
+                   user_id=3)
+
+# Add new users to session
+db.session.add(doe_post)
+db.session.add(grohl_post)
+db.session.add(warren_post)
 
 # Commit to DB
 db.session.commit()
