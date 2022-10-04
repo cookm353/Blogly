c9f1122c95174a0b8f42407655cdfe49014537d3
Matthew Cook <cookm353@gmail.com>
2022-10-04 16:46:38 -0400

    adding tags and post tags

--- seed.py
+++ seed.py
@@ -1,4 +1,4 @@
-from models import User, Post, db
+from models import User, Post, Tag, PostTag, db
 from app import app
 
 
@@ -6,30 +6,27 @@ from app import app
 db.drop_all()
 db.create_all()
 
-# Ensure it's empty
+# Ensure tables are empty
 User.query.delete()
+Post.query.delete()
+Tag.query.delete()
+PostTag.query.delete()
 
+"""
+Users
+"""
 # Add starting users
-jackie_chan = User(first_name='Jackie', last_name='Chan',
+jackie = User(first_name='Jackie', last_name='Chan',
             img_url='static/jackie_chan.jpg')
-dave_grohl = User(first_name='Dave', last_name='Grohl',
+grohl = User(first_name='Dave', last_name='Grohl',
             img_url='static/dave_grohl.png')
 warren = User(first_name='Elizabeth', last_name='Warren',
             img_url='static/elizabeth_warren.jpg')
 doe = User(first_name='Jane', last_name='Doe')
 
-# Add new users to session
-db.session.add(jackie_chan)
-db.session.add(dave_grohl)
-db.session.add(warren)
-db.session.add(doe)
-
-# Commit to DB
-db.session.commit()
-
-# Ensure Posts table is empty
-Post.query.delete()
-
+"""
+Posts
+"""
 # Add starting users
 doe_post = Post(title='First Post!', 
                  content='Hello world!', user_id=4)
@@ -37,13 +34,26 @@ grohl_post = Post(title='Newest Album',
                   content='Check out our latest album, Medicine at Midnight!',
                   user_id=2)
 warren_post = Post(title='Have a Problem?',
-                   content='I have a plant for that!',
+                   content='I have a plan for that!',
                    user_id=3)
 
-# Add new users to session
-db.session.add(doe_post)
-db.session.add(grohl_post)
-db.session.add(warren_post)
-
-# Commit to DB
+# Add posts to DB
+db.session.add_all([jackie, grohl, warren, doe, doe_post, 
+                    grohl_post, warren_post])
 db.session.commit()
+
+"""
+Tags
+"""
+# Add a few tags
+funny_tag = Tag(tag_name='Funny', posttag=[PostTag(post_id=3)])
+hi_tag = Tag(tag_name='Hi', posttag=[PostTag(post_id=1)])
+wow_tag = Tag(tag_name='Wow!', posttag=[PostTag(post_id=2)])
+news_tag = Tag(tag_name='News', posttag=[PostTag(post_id=2), PostTag(post_id=3)])
+
+# Add tags and users to DB
+db.session.add_all([funny_tag, hi_tag, wow_tag, news_tag])
+db.session.commit()
+
+
+# Posts -> Post Tags -> Tags
