bc8cc42a33be82ecbcdc533b24671a2ef3e50953
Matthew Cook <cookm353@gmail.com>
2022-10-07 11:26:04 -0400

    tests complete(ish)

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from app import app
-from models import User, Post, db
+from models import User, Post, Tag, PostTag, db
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
 app.config['SQLALCHEMY_ECHO'] = True
@@ -23,27 +23,31 @@ class Test_App(TestCase):
         db.session.add(post)
         db.session.commit()
         
+        Tag.query.delete()
+        
+        tag = Tag(tag_name='Wow!', posttag=[PostTag(post_id=1)])
+        db.session.add(tag)
+        db.session.commit()
+        
         self.user_id = user.user_id
         self.user = user      
-        # self.post_id = post.post_id
-        # self.post = post
+        self.post_id = post.post_id
+        self.post = post
         
     def tearDown(self):
         db.session.rollback()
         
-    def test_index_redirect(self):
+    def test_home(self):
         with app.test_client() as client:
             resp = client.get('/')
-            
-            self.assertEqual(resp.status_code, 302)
-            
-    def test_index(self):
-        with app.test_client() as client:
-            resp = client.get('/', follow_redirects=True)
             html = resp.get_data(as_text=True)
             
             self.assertEqual(resp.status_code, 200)
-            
+            self.assertIn('Blogly Recent Posts', html)
+            self.assertIn('Wow', html)
+        
+    """User tests"""
+
     def test_user_display_list(self):
         with app.test_client() as client:
             resp = client.get('/users')
@@ -60,24 +64,31 @@ class Test_App(TestCase):
             self.assertEqual(resp.status_code, 200)
             
     def test_adding_users(self):
-        # Just doesn't want to work for me
         with app.test_client() as client:
             d = {'first_name': 'Jane', 'last_name': 'Doe', 
                  'img_url': 'static/default_user.png'}
-            resp = client.post('/users/new', data=d)
+            resp = client.post('/users/new', data=d,
+                               follow_redirects=True)
+            html = resp.get_data(as_text=True)
             
-            # html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Doe', html)
             
-            self.assertEqual(resp.status_code, 302)
-            # self.assertIn('Jane, Doe', html)
-            
-    def test_showing_details(self):
+    def test_showing_user_details(self):
         with app.test_client() as client:
             resp = client.get('/users/1')
             html = resp.get_data(as_text=True)
             
             self.assertEqual(resp.status_code, 200)
             self.assertIn('John Doe', html)
+            
+    def test_update_user_form(self):
+        with app.test_client() as client:
+            resp = client.get('/users/1/edit')
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Doe', html)
             
     def test_deleting_user(self):
         with app.test_client() as client:
@@ -95,6 +106,8 @@ class Test_App(TestCase):
             self.assertEqual(resp.status_code, 200)
             self.assertNotIn('John Doe', html)
             
+    """Post Tests"""        
+    
     def test_showing_posts(self):
         with app.test_client() as client:
             resp = client.get('/users/1')
@@ -106,12 +119,14 @@ class Test_App(TestCase):
     def test_showing_new_post_form(self):
         with app.test_client() as client:
             resp = client.get('/users/1/posts/new')
+            html = resp.get_data(as_text=True)
             
             self.assertEqual(resp.status_code, 200)
+            self.assertIn('Wow', html)
         
     def test_creating_post(self):
         with app.test_client() as client:
-            data = {'post_title': 'Hello', 'post_content': 'World'}
+            data = {'post_title': 'Hello', 'post_content': 'World', 'tag1': '1'}
             resp = client.post('/users/1/posts/new', data=data,
                                follow_redirects=True)
             html = resp.get_data(as_text=True)
@@ -126,6 +141,7 @@ class Test_App(TestCase):
             
             self.assertEqual(resp.status_code, 200)
             self.assertIn('Bar', html)
+            self.assertIn('Wow', html)
         
     def test_edit_post_form(self):
         with app.test_client() as client:
@@ -134,8 +150,9 @@ class Test_App(TestCase):
             
             self.assertEqual(resp.status_code, 200)
             self.assertIn('Edit Post', html)
+            self.assertIn('Wow', html)
             
-    def test_editting_post(self):
+    def test_editing_post(self):
         with app.test_client() as client:
             data = {'post_title': 'Hello', 'post_content': 'World'}
             resp = client.post('/posts/1/edit', data=data,
@@ -143,7 +160,8 @@ class Test_App(TestCase):
             html = resp.get_data(as_text=True)
             
             self.assertEqual(resp.status_code, 200)
-            self.assertIn('Hello', html)
+            self.assertIn('World', html)
+            self.assertIn('Wow', html)
             self.assertNotIn('Foo', html)
         
     def test_deleting_post(self):
@@ -154,3 +172,77 @@ class Test_App(TestCase):
             self.assertEqual(resp.status_code, 200)
             self.assertIn('Posts', html)
             self.assertNotIn('Foo', html)
+            
+    """Tag tests"""
+            
+    def test_tag_list(self):
+        with app.test_client() as client:
+            resp = client.get('/tags')
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Tags', html)
+            self.assertIn('Wow!', html)
+            
+    def test_tag_details(self):
+        with app.test_client() as client:
+            resp = client.get('/tags/1')
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Wow', html)
+            self.assertIn('Foo', html)
+            
+    def test_missing_tag_details(self):
+        with app.test_client() as client:
+            resp = client.get('/tags/2')
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 404)
+            
+    def test_new_tag_form(self):
+        with app.test_client() as client:
+            resp = client.get('/tags/new')
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Create a Tag', html)
+            
+    def test_make_new_tag(self):
+        with app.test_client() as client:
+            data = {'tag_name': 'Triple Bam!'}
+            
+            resp = client.post('/tags/new', follow_redirects=True,
+                               data=data)
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Triple Bam!', html)
+            
+    def test_edit_tag_page(self):
+        with app.test_client() as client:
+            resp = client.get('/tags/1/edit')
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Edit a Tag', html)
+            
+    def test_editing_tag(self):
+        with app.test_client() as client:
+            data = {'new_tag_name': 'Triple Bam!'}
+            resp = client.post('/tags/1/edit', follow_redirects=True,
+                               data=data)
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Triple Bam!', html)
+            self.assertNotIn('Wow', html)
+            
+    def test_tag_delete(self):
+        with app.test_client() as client:
+            resp = client.post('/tags/1/delete', follow_redirects=True)
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertNotIn('Wow', html)
+            self.assertIn('Tags', html)
